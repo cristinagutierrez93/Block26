ea23928bc5672a3deab6649677c1cf995685529b
Cristina Gutierrez <cristinagutierrez@Cristinas-MacBook-Pro.local>
2023-06-08 03:14:14 -0500

    block26

---
+++ Block26/src/App.jsx
@@ -0,0 +1,21 @@
+import ContactList from './components/ContactList'
+import "./App.css";
+import { useState } from "react";
+import SelectedContact from "./components/SelectedContact";
+
+
+
+  function App() {
+    const [selectedContactId, setSelectedContactId]= useState(null);
+  return (
+    <>
+    {selectedContactId ? (
+      <div> Selected Contact View</div>
+    ) : (
+      <ContactList />
+    )}
+    </>
+  );
+}
+
+export default App
